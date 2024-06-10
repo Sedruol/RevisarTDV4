2dffbe1d790858c3b5fb64c84a618356756f85dd
Sedruol <destructor2104992013@gmail.com>
2024-06-09 19:49:20 -0500

    Fix T1

--- Assets/T1/Scripts/Question14.cs
+++ Assets/T1/Scripts/Question14.cs
@@ -6,24 +6,23 @@ public class Question14 : MonoBehaviour
 {
     public int number1 = 3;
     public int number2 = 5;
-    void CalculateFactorialNumber(int n1, int n2)
+    int result1;
+    int result2;
+    int result;
+    int CalculateFactorialNumber(int n)
     {
-        int result1 = 1;
-        int result2 = 1;
-        int result = 0;
-        for (int i = 1; i <= n1; i++)
+        int temp = 1;
+        for (int i = 1; i <= n; i++)
         {
-            result1 *= i;
+            temp *= i;
         }
-        for (int j = 1; j <= n2; j++)
-        {
-            result2 *= j;
-        }
-        result = result1 + result2;
-        Debug.Log(number1 + "! + " + number2 + "! = " + result);
+        return temp;
     }
     void Start()
     {
-        CalculateFactorialNumber(number1, number2);
+        result1 = CalculateFactorialNumber(number1);
+        result2 = CalculateFactorialNumber(number2);
+        result = result1 + result2;
+        Debug.Log(number1 + "! + " + number2 + "! = " + result);
     }
 }
